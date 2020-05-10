1dc8f68ae569d1199a0f74acc3886ab84fc38ae0
Gagan Malvi <malvigagan@gmail.com>
2020-05-10 09:39:09 +0000

    Modify for Cartel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MoeSyndrome Kernel for Redmi Note 4(X) Snapdragon
+kernel.string=Cartel Kernel for Redmi Note 4(X) Snapdragon
 do.devicecheck=1
 do.modules=0
 do.systemless=0
